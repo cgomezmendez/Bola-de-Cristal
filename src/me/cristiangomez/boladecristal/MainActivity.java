7a63d026b92200a0a2ceea0f7bf0ff567ae875b8
Cristian Gomez <contact@cristiangomez.me>
2014-06-12 15:39:33 -0400

    added accelerometer sensor and deleted button

--- src/me/cristiangomez/boladecristal/MainActivity.java
+++ src/me/cristiangomez/boladecristal/MainActivity.java
@@ -2,21 +2,25 @@ package me.cristiangomez.boladecristal;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.crystalball.ShakeDetector;
 
 public class MainActivity extends Activity {
 	private CrystalBall mCrystalBall = new CrystalBall();
 	private TextView mResponseLabel;
-	private Button mGetResponseButton;
 	private ImageView mCrystallBallImage;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +28,18 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		mCrystallBallImage = (ImageView) findViewById(R.id.imageView1);
 		mResponseLabel = (TextView) findViewById(R.id.responseLabel);
-		mGetResponseButton = (Button) findViewById(R.id.getResponseButton);
-		mGetResponseButton.setOnClickListener(new View.OnClickListener() {
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
+
 			@Override
-			public void onClick(View getResponseButtonView) {
-				// The button was clicked so we updated the responseLabel with
-				// an response
-				String response = "";
-				response = mCrystalBall.getAnswer();
-				mResponseLabel.setText(response);
-				playSound();
-				animateCrystalBall();
-				animateAnswer();
+			public void onShake() {
+				handleNewAnswer();
+
 			}
 		});
+
 	}
 
 	private void animateCrystalBall() {
@@ -88,5 +90,14 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private void handleNewAnswer() {
+		String response = "";
+		response = mCrystalBall.getAnswer();
+		mResponseLabel.setText(response);
+		animateCrystalBall();
+		animateAnswer();
+		playSound();
 	}
 }
